7b14b979f7f4fbe20cac38e35f555ce1a9824136
nickhitos <nickhit3802@gmail.com>
2024-08-27 16:16:03 -0700

    use palindrome function

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,14 @@
 # palindrome.py
 
 def is_palindrome(s: str) -> bool:
-    """
-    Check if the given string is a palindrome.
-    
-    A palindrome is a word that reads the same backward as forward.
-    """
-    s = s.lower().replace(" ", "")
-    return s == s[::-1]
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while r > l and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l, r = l + 1, r - 1
+    return True
