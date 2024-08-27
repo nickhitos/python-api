f6948fcf2c09695a22614903c8f850e10c751d1d
nickhitos <nickhit3802@gmail.com>
2024-08-27 16:27:07 -0700

    add more tests

--- test_palindrome.py
+++ test_palindrome.py
@@ -18,3 +18,28 @@ def test_case_insensitivity():
 def test_spaces():
     assert is_palindrome("A man a plan a canal Panama")
     assert not is_palindrome("This is not a palindrome")
+
+def test_single_character():
+    assert is_palindrome("a")
+    assert is_palindrome("A")
+    assert is_palindrome(" ")
+
+def test_special_characters():
+    assert is_palindrome("$%*([])")
+    assert is_palindrome("Madam, in Eden, I'm Adam")
+    assert is_palindrome("No lemon, no melon!")
+
+def test_numbers():
+    assert is_palindrome("12321")
+    assert not is_palindrome("12345")
+
+def test_long_palindrome():
+    long_palindrome = "a" * 10000 + "b" + "a" * 5000
+    assert not is_palindrome(long_palindrome)
+
+    long_palindrome = "a" * 10000 + "a" * 10000
+    assert is_palindrome(long_palindrome)
+
+def test_unicode():
+    assert is_palindrome("😊a😊")
+    assert not is_palindrome("a😊b😊")
