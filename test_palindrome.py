bbe7ac402279c697e08f6e9d6bcdd2e29fdda08b
nickhitos <nickhit3802@gmail.com>
2024-08-27 15:58:44 -0700

    use pytest

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,23 +1,20 @@
-import unittest
+# test_palindrome.py
+
 from palindrome import is_palindrome
 
-class TestPalindrome(unittest.TestCase):
-    def test_basic_palindromes(self):
-        self.assertTrue(is_palindrome("madam"))
-        self.assertTrue(is_palindrome("racecar"))
-        self.assertTrue(is_palindrome(""))
+def test_basic_palindromes():
+    assert is_palindrome("madam")
+    assert is_palindrome("racecar")
+    assert is_palindrome("")
 
-    def test_non_palindromes(self):
-        self.assertFalse(is_palindrome("hello"))
-        self.assertFalse(is_palindrome("world"))
+def test_non_palindromes():
+    assert not is_palindrome("hello")
+    assert not is_palindrome("world")
 
-    def test_case_insensitivity(self):
-        self.assertTrue(is_palindrome("Madam"))
-        self.assertTrue(is_palindrome("RaceCar"))
+def test_case_insensitivity():
+    assert is_palindrome("Madam")
+    assert is_palindrome("RaceCar")
 
-    def test_spaces(self):
-        self.assertTrue(is_palindrome("A man a plan a canal Panama"))
-        self.assertFalse(is_palindrome("This is not a palindrome"))
-
-if __name__ == '__main__':
-    unittest.main()
+def test_spaces():
+    assert is_palindrome("A man a plan a canal Panama")
+    assert not is_palindrome("This is not a palindrome")
